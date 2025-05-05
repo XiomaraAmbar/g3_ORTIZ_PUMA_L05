9dd7899cb32598d829b8fb488774462a2cebb06c
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-05-05 01:24:23 -0500

    Adición de métodos detele por valor y por posición del nodo

--- LinkedList/ListaEnlazada.java
+++ LinkedList/ListaEnlazada.java
@@ -49,7 +49,7 @@ public class ListaEnlazadaCabecera<E extends Nodo<E>>{ //Hereda de la clase Nodo
         //return -1;//Caso contrario retorna -1, no se encontro el elemento o  el nodo
     }
 
-    //Busca el k-ésimo elemento
+    //Busca el k-ésimo elemento por su posición y retorna el nodo (valor)
     public E searchK(int k) throws MensajeException{
         if (cabecera.getSiguiente() == null){ //Verifica si la lista esta vacía
             //Si esta vacía lanza una excepción
@@ -69,20 +69,24 @@ public class ListaEnlazadaCabecera<E extends Nodo<E>>{ //Hereda de la clase Nodo
     }
 
     //Inserta el nuevo nodo al inicio de la lista
-    public void insertFirst(E nuevo){ 
+    public void insertFirst(E nuevo){
         Nodo<E> nuevoNodo = new Nodo<E> (nuevo); //Se crea un nuevo nodo con el valor del nuevo elemento
         nuevoNodo.setSiguiente(cabecera.getSiguiente()); // El nuevo nodo apunta al siguiente de cabecera
         cabecera.setSiguiente(nuevoNodo); //Y la cabecera apunta al nuevo nodo
     }
 
     //Inserta el nuevo nodo a una posicion x en la lista
-    public void insertPosicionK(E nuevo, int posicionK){
-        Nodo<E> nuevoNodo = new Nodo<E> (nuevo);
-        if (cabecera.getSiguiente() == null){
-            cabecera.setSiguiente(nuevoNodo);
+    public void insertPosicionK(E nuevo, int posicionK) throws MensajeException{
+        Nodo<E> nuevoNodo = new Nodo<E> (nuevo); //Se crea un nuevo nodo con el valor del nuevo elemento
+        Nodo<E> nodoAnterior = searchK(posicionK-1); //El nodo anterior es el nodo anterior al nodo del medio
+        Nodo<E> nodoMedio = searchK(posicionK); //El nodo del medio se refiere a el nodo el cual se desplazara de su posición
+        if (cabecera.getSiguiente() == null){ //Si la lista esta vacía
+            insertFirst(nuevo); //Se inserta el nuevo nodo al inicio
         }
-        nuevoNodo.setSiguiente(cabecera.getSiguiente());
-        cabecera.setSiguiente(nuevoNodo);
+        else{ //Caso contrario
+            nuevoNodo.setSiguiente(nodoMedio); //El nuevo nodo apuntara al nodo medio (nodo desplazado)
+            nodoAnterior.setSiguiente(nuevoNodo); //El nodo anterior apuntara al nuevo nodo
+        }
     }
 
     //Inserta el nuevo nodo al final de la lista
@@ -100,8 +104,27 @@ public class ListaEnlazadaCabecera<E extends Nodo<E>>{ //Hereda de la clase Nodo
         }
     }
 
-    //Elimina un nodo de la lista enlazada
-    public void removeNode(){
+    //Elimina un nodo de la lista enlazada por contenido (valor)
+    public void removeNode(E nodo) throws MensajeException{
+        if (cabecera.getSiguiente() == null){ //Verifica si la lista esta vacía
+            //Si esta vacía lanza una excepción
+            throw new MensajeException("Lista enlazada vacía, no hay elementos que eliminar.");
+        }
+        int posicionNodo = search(nodo); //La variable posicionNodo sera la posicion del nodo a eliminar en la lista
+        Nodo<E> nodoEliminado = searchK(posicionNodo); //nodoEliminado es el nodo en la posicion del nodo que se quiere eliminar
+        Nodo<E> nodoAnterior = searchK(posicionNodo-1); //El nodo anterior es el nodo anterior que apunta al nodo que se quiere eliminar
+        nodoAnterior.setSiguiente(nodoEliminado.getSiguiente()); //El nodo anterior al nodo eliminado apunta al siguiente del nodo eliminado
+    }
+
+    //Elimina un nodo de la lista enlazada por posición
+    public void removeNodeK(int posicionK) throws MensajeException{
+        if (cabecera.getSiguiente() == null){ //Verifica si la lista esta vacía
+            //Si esta vacía lanza una excepción
+            throw new MensajeException("Lista enlazada vacía, no hay elementos que eliminar.");
+        }
+        Nodo<E> nodoEliminado = searchK(posicionK); //nodoEliminado es el nodo en la posicion del nodo que se quiere eliminar
+        Nodo<E> nodoAnterior = searchK(posicionK-1); //El nodo anterior es el nodo anterior que apunta al nodo que se quiere eliminar
+        nodoAnterior.setSiguiente(nodoEliminado.getSiguiente()); //El nodo anterior al nodo eliminado apunta al siguiente del nodo eliminado
     }
 
     //Imprime la lista enlazada
